efdc8688c0f4719c84359cb6683d995e8f783668
Andrew Gerrand <adg@golang.org>
2022-10-27 09:44:39 +1100

    Add OnceValue and OnceValueErr

--- once.go
+++ once.go
@@ -17,3 +17,28 @@ func OnceFunc[T any](fn func() (T, error)) func() (T, error) {
 		return value, err
 	}
 }
+
+type OnceValue[T any] struct {
+	once sync.Once
+	v    T
+}
+
+func (o *OnceValue[T]) Do(fn func() T) T {
+	o.once.Do(func() {
+		o.v = fn()
+	})
+	return o.v
+}
+
+type OnceValueErr[T any] struct {
+	once sync.Once
+	v    T
+	err  error
+}
+
+func (o *OnceValueErr[T]) Do(fn func() (T, error)) (T, error) {
+	o.once.Do(func() {
+		o.v, o.err = fn()
+	})
+	return o.v, o.err
+}
